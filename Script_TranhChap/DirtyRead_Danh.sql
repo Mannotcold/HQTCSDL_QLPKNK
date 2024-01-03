af55f91f49a281a4b4bf0d40d1a1f3a8a41a407c
Mannotcold <man2082002@gmail.com>
2024-01-03 23:58:09 +0700

    up

---
+++ Script_TranhChap/DirtyRead_Danh.sql
@@ -0,0 +1,61 @@
+﻿
+
+
+CREATE PROC Sp_XemThongTinCaNhan
+       @MaKH int,
+	   @HoTenKH varchar(50),
+	   @NgaySinhKH datetime,
+	   @DiaChiKH varchar(50),
+	   @SDTKH int,
+	   @EmailKH varchar(50),
+	   @MaNV int
+AS
+BEGIN TRAN 
+     
+	 BEGIN TRY
+
+	 IF EXISTS (SELECT * FROM KHACHHANG WHERE SDTKH = @SDTKH)
+BEGIN
+       PRINT N'Không tồn tài khoản'
+       ROLLBACK TRAN
+       RETURN 0
+
+END
+
+	 
+
+	UPDATE KHACHHANG SET EmailKH = @EmailKH Where SDTKH = @SDTKH;
+	 WAITFOR DELAY '0:0:10';
+	 END TRY 
+	 BEGIN CATCH
+	       
+		   PRINT N'Lỗi hệ thống'
+	       ROLLBACK TRAN
+	 END CATCH
+COMMIT TRAN
+
+CREATE PROC Sp_XemThongTinKhachHang
+       @MaKH int
+AS
+BEGIN TRAN 
+         SET TRAN ISOLATION LEVEL READ UNCOMMITTED
+	 BEGIN TRY
+
+	 IF EXISTS (SELECT * FROM KHACHHANG WHERE MaKH = @MaKH)
+BEGIN
+         PRINT N'Không tồn khách hàng đó'
+         ROLLBACK TRAN
+         RETURN 0
+END
+
+	 
+
+	 SELECT * FROM KHACHHANG Where MaKH = @MaKH;
+	 
+	 END TRY 
+	 BEGIN CATCH
+	       
+		   PRINT N'Lỗi hệ thống'
+	       ROLLBACK TRAN
+	 END CATCH
+COMMIT TRAN
---
+++ Script_TranhChap/PhanTomRead_Danh.sql
@@ -0,0 +1,44 @@
+﻿--Phantom Read Loi
+CREATE PROCEDURE Sp_ThemLichHen_1 @NgayHen datetime, @ThoiGianHen time, @HoTenKH NVARCHAR(50), @MaKH int, @MaNS int
+AS
+
+BEGIN TRAN
+	BEGIN TRY
+	--Kiểm tra cuộc hẹn có trùng hay không
+	IF(EXISTS(SELECT * FROM LichHen  WHERE NgayHen = ‘01-10-2023’ AND ThoiGianHen = '08:30:00') 
+RETURN -1
+
+
+	-- Kiểm tra mã nha sĩ có trùng hay không
+	IF(EXISTS(SELECT * FROM LichHen  WHERE MaNS = ‘1’ ) 
+    RETURN -1
+	
+	INSERT INTO LichHen(NgayHen, ThoiGianHen, TenKH, MaKH, MaNS) VALUES (‘01-10-2023’, ‘08:30:00’, N’Nguyen Van E’, ’ 3’, ‘2’) 
+return 1
+	END TRY
+	BEGIN CATCH
+		PRINT N'LỖI HỆ THỐNG'
+		ROLLBACK TRAN
+		RETURN 1
+	END CATCH
+COMMIT TRAN
+	return 1
+GO
+
+--PROCEDURE khách hàng xem danh sách đối tác
+CREATE PROCEDURE sp_KH_XemDSDoiTac
+AS
+
+BEGIN TRAN
+	BEGIN TRY
+		SELECT Ngay,ThoiGian,MaNS FROM LichCaNhanNS 
+		WAITFOR DELAY '0:0:10'
+	END TRY
+	BEGIN CATCH
+			PRINT N'LỖI HỆ THỐNG'
+			ROLLBACK TRAN
+			RETURN 0
+	END CATCH
+COMMIT TRAN
+return 1
+GO
